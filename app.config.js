85fefc227a8b35b20475742c657114a219a2c241
seyoung Ko <tpdud3256@gmail.com>
2023-11-18 01:54:27 +0900

    expo sdk 업그레이드

--- app.config.js
+++ app.config.js
@@ -33,7 +33,6 @@ export default {
                 {
                     isAndroidBackgroundLocationEnabled: true,
                 },
-                "@notifee/react-native",
             ],
         ],
     },
